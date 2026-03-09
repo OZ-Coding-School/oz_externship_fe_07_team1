7ad50fdbb178ec2a17c8513f4d454a6bcbcc6dcc
sysysysyb <baeksy6067@gmail.com>
2026-03-09 16:30:48 +0900

    refactor: Test 페이지 수정 (#16)
    - 테스트 페이지에 TestWrapper 컴포넌트를 사용해서 UI를 추가하도록 수정

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,13 +1,16 @@
+import TestWrapper from '../components/test/TestWrapper'
+
 function Test() {
   return (
     <div className="flex h-dvh flex-col items-center gap-10 overflow-y-auto bg-pink-100 p-10">
       <h1 className="text-4xl font-bold text-gray-700">
         Test 페이지 ^~^ (UI 확인용)
       </h1>
-      <div className="flex flex-col items-center gap-4 rounded-xl border-4 border-solid border-pink-300 bg-white p-8">
-        <h3 className="text-2xl font-medium">컴포넌트 이름 (EX. Button)</h3>
-        <div>(여기에 컴포넌트 추가!)</div>
-      </div>
+      <TestWrapper label="테스트">
+        <button className="rounded-md bg-pink-100 px-4 py-2 text-gray-700 hover:bg-pink-200">
+          test
+        </button>
+      </TestWrapper>
     </div>
   )
 }
