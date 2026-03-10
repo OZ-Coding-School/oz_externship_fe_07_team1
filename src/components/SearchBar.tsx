1d22d893eda6b9cfad4dead91c868d67d53e65e2
김우진 <kedw158@gmail.com>
2026-03-10 15:16:02 +0900

    fix: 피드백 수용 후 수정 (#19)

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,14 +2,13 @@ import { Search, XCircle } from 'lucide-react'
 import { forwardRef, type ComponentProps, useState } from 'react'
 import { cn } from '../lib/utils'
 
-interface SearchBarProps extends ComponentProps<'div'> {
+interface SearchBarProps extends Omit<ComponentProps<'input'>, 'onChange'> {
   value?: string
-  onSearch?: (value: string) => void
-  onClear?: () => void
+  onValueChange?: (value: string) => void
 }
 
 const SearchBar = forwardRef<HTMLDivElement, SearchBarProps>(
-  ({ className, value, onSearch, onClear, ...props }, ref) => {
+  ({ className, value, onValueChange, ...props }, ref) => {
     const [isFocused, setIsFocused] = useState(false)
 
     return (
@@ -35,18 +34,19 @@ const SearchBar = forwardRef<HTMLDivElement, SearchBarProps>(
             // 포커스: 클릭했을 때 상태 변경
             onFocus={() => setIsFocused(true)}
             onBlur={() => setIsFocused(false)}
-            onChange={(e) => onSearch?.(e.target.value)}
-            // // 디폴트
-            // // 텍스트넣은후
+            onChange={(e) => onValueChange?.(e.target.value)}
+            // 디폴트
+            // 텍스트넣은후
             className="text-grey-6 placeholder:text-grey-4 w-full border-none bg-transparent text-sm font-medium outline-none"
           />
         </div>
 
         {isFocused && (
           <button
+            type="button"
             onMouseDown={(e) => {
               e.preventDefault()
-              onClear?.()
+              onValueChange?.('')
             }}
             className="text-grey-3 hover:text-grey-4 flex items-center justify-center"
           >
