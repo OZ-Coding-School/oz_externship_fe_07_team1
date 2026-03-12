2b06fc2f537b7f41e31aaefdb883dcf726962c9e
sysysysyb <baeksy6067@gmail.com>
2026-03-12 16:16:44 +0900

    feat: PostCard props 추가 (#34)
    - props 기본값으로 postCardData 더미데이터 지정

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,22 +1,28 @@
 import { ThumbsUp } from 'lucide-react'
+import { postCardData } from '../mocks/data/postCardData'
 
-function PostCard() {
+function PostCard({
+  category = postCardData.categoryName,
+  title = postCardData.title,
+  contentPreview = postCardData.contentPreview,
+  likeCount = postCardData.likeCount,
+  viewCount = postCardData.viewCount,
+  commentCount = postCardData.commentCount,
+  authorName = postCardData.author.nickname,
+  authorProfileImg = postCardData.author.profileImgUrl,
+}) {
   return (
     <div className="hover:bg-primary-50 flex h-56 w-full cursor-pointer gap-10 rounded-xl p-6 transition-all duration-200">
       <div className="flex w-full flex-col justify-between">
         <div className="flex flex-col gap-3">
           {/* 카테고리 */}
-          <span className="text-xs text-gray-600">구인/협업</span>
+          <span className="text-xs text-gray-600">{category}</span>
 
           {/* 제목 */}
-          <h3 className="truncate text-lg font-semibold text-black">
-            데이터 분석 프로젝트 구합니다!
-          </h3>
+          <h3 className="truncate text-lg font-semibold text-black">{title}</h3>
 
           {/* 내용 (한 줄) */}
-          <p className="truncate text-sm text-gray-400">
-            저는 완전 기초인데 혹시 같이 프로젝트 만드실분 계신가요?!
-          </p>
+          <p className="truncate text-sm text-gray-400">{contentPreview}</p>
         </div>
 
         <div className="flex w-full justify-between">
@@ -24,13 +30,13 @@ function PostCard() {
           <div className="flex gap-3">
             <div className="flex items-center gap-1 text-gray-500">
               <ThumbsUp className="size-5" />
-              <span className="text-xs">좋아요 156</span>
+              <span className="text-xs">좋아요 {likeCount}</span>
             </div>
             <div className="flex items-center gap-1 text-gray-500">
-              <span className="text-xs">댓글 6</span>
+              <span className="text-xs">댓글 {commentCount}</span>
             </div>
             <div className="flex items-center gap-1 text-gray-500">
-              <span className="text-xs">조회수 60</span>
+              <span className="text-xs">조회수 {viewCount}</span>
             </div>
           </div>
 
@@ -38,11 +44,11 @@ function PostCard() {
             {/* 프로필 사진 / 이름 */}
             <div className="flex items-center gap-1">
               <img
-                src="https://picsum.photos/200/300"
-                alt="조조야-profile-image"
+                src={authorProfileImg}
+                alt="author-profile-image"
                 className="size-6 rounded-full object-cover"
               />
-              <span className="text-xs text-gray-600">조조야</span>
+              <span className="text-xs text-gray-600">{authorName}</span>
             </div>
 
             {/* 작성 시기 */}
