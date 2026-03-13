8bd3893637a2c203ea70fcc82d2b112cd0cd2db6
sysysysyb <baeksy6067@gmail.com>
2026-03-13 17:00:00 +0900

    feat: useSlider 적용으로 반응형 대응 및 스타일링 추가 (#42)
    - useSlider hook 리턴값 활용
    - 화면 너비에 동적으로 대응하기 위해 shrink-0 등 스타일링 추가 지정

--- src/components/CategoryFilterBar.tsx
+++ src/components/CategoryFilterBar.tsx
@@ -1,4 +1,6 @@
 import { cn } from '../lib/utils'
+import { ChevronLeft, ChevronRight } from 'lucide-react'
+import { useSlider } from '../hooks/useSlider'
 
 type categoryDataType = { id: number; name: string }
 
@@ -13,25 +15,58 @@ function CategoryFilterBar({
   onCategoryClick,
   categoryList,
 }: CategoryFilterBarProps) {
-  const handleButtonClick = (value: categoryDataType) => {
-    onCategoryClick(value)
-  }
+  const {
+    sliderRef,
+    trackRef,
+    translateX,
+    canSlideLeft,
+    canSlideRight,
+    slideLeft,
+    slideRight,
+  } = useSlider()
 
   return (
     <div className="flex gap-1">
-      {categoryList.map((el) => (
-        <button
-          key={el.id}
-          className={cn(
-            'hover:bg-primary-100 cursor-pointer rounded-sm p-5 text-base font-semibold text-gray-600 transition-all duration-200',
-            el.id === currentCategory.id &&
-              'text-primary-default bg-primary-100'
-          )}
-          onClick={() => handleButtonClick(el)}
+      {/* 이전 버튼 */}
+      <button
+        className="group cursor-pointer"
+        onClick={slideLeft}
+        disabled={!canSlideLeft}
+      >
+        <ChevronLeft className="group-hover:text-primary-default group-disabled:text-gray-disabled size-6 text-gray-600" />
+      </button>
+
+      {/* 카테고리 탭 */}
+      <div ref={sliderRef} className="flex-1 overflow-hidden">
+        <div
+          ref={trackRef}
+          className="flex gap-1 transition-transform duration-300"
+          style={{ transform: `translateX(-${translateX}px)` }}
         >
-          {el.name}
-        </button>
-      ))}
+          {categoryList.map((el) => (
+            <button
+              key={el.id}
+              className={cn(
+                'hover:bg-primary-100 transition-color shrink-0 cursor-pointer rounded-sm px-5 py-3 text-base font-semibold text-gray-600 duration-200',
+                el.id === currentCategory.id &&
+                  'text-primary-default bg-primary-100'
+              )}
+              onClick={() => onCategoryClick(el)}
+            >
+              {el.name}
+            </button>
+          ))}
+        </div>
+      </div>
+
+      {/* 다음 버튼 */}
+      <button
+        className="group cursor-pointer"
+        onClick={slideRight}
+        disabled={!canSlideRight}
+      >
+        <ChevronRight className="group-hover:text-primary-default group-disabled:text-gray-disabled size-6 text-gray-600" />
+      </button>
     </div>
   )
 }
