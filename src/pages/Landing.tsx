565bde502beb3c9b181a0aa95a278905b7e8e968
sysysysyb <baeksy6067@gmail.com>
2026-03-16 14:49:25 +0900

    chore: 임시 랜딩 페이지 삭제 (#48)

--- src/pages/Landing.tsx
+++
@@ -1,9 +0,0 @@
-function Landing() {
-  return (
-    <div className="h-250 w-full p-20 text-center">
-      <h1 className="text-3xl">임시 랜딩 페이지 입니다!</h1>
-    </div>
-  )
-}
-
-export default Landing
