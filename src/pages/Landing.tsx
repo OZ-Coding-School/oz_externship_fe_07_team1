b9d1a5e0d721c0ef430ada319942d3f77a98c8d9
sysysysyb <baeksy6067@gmail.com>
2026-03-16 14:03:57 +0900

    refactor: 임시 랜딩 페이지 컴포넌트 수정 (#48)
    - RootLayout 적용 확인을 위해 스타일링 수정

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,7 +1,7 @@
 function Landing() {
   return (
-    <div>
-      <h1>커뮤니티 파트 랜딩 페이지 입니다!</h1>
+    <div className="h-250 w-full p-20 text-center">
+      <h1 className="text-3xl">임시 랜딩 페이지 입니다!</h1>
     </div>
   )
 }
