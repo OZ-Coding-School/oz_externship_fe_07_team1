c2c3736b7ac772cb174d92d2365909b35bd78c4e
김우진 <kedw158@gmail.com>
2026-03-23 15:42:25 +0900

    refactor: post.ts 수정 및 title 추가 (#64)

--- src/types/api-request-types/post.ts
+++ src/types/api-request-types/post.ts
@@ -1,10 +1,26 @@
+export interface PostCategory {
+  id: number
+  name: string
+}
+
 export interface CreatePostRequest {
   title: string
   content: string
   category_id: number
 }
+
 export interface UpdatePostRequest {
   title: string
   content: string
   category_id: number
+}
+
+export interface PostDetailResponse {
+  id: number
+  title: string
+  content: string
+  name: string
+  category_id: number
+  created_at: string
+  updated_at: string
 }
