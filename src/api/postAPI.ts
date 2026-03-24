071f6277b7fef1a51ee649cde13cb75c74e67489
김우진 <kedw158@gmail.com>
2026-03-24 16:04:01 +0900

    refactor: .env에 맞춰 post.ts 경로 수정 (#70)

--- src/api/postAPI.ts
+++ src/api/postAPI.ts
@@ -16,7 +16,7 @@ async function getPostsAPI(params: {
   category_id?: number
   sort?: 'latest' | 'oldest' | 'most_views' | 'most_likes' | 'most_comments'
 }) {
-  const response = await instance.get('/api/v1/posts/', {
+  const response = await instance.get('/posts/', {
     params,
   })
   return response.data
@@ -24,44 +24,37 @@ async function getPostsAPI(params: {
 
 // 카테고리 목록 조회
 async function getPostCategoriesAPI() {
-  const response = await instance.get<PostCategory[]>(
-    '/api/v1/posts/categories'
-  )
+  const response = await instance.get<PostCategory[]>('/posts/categories')
   return response.data
 }
 
 // 게시글 생성
 async function createPostAPI(params: CreatePostRequest) {
-  const response = await instance.post<CreatePostResponse>(
-    '/api/v1/posts/',
-    params
-  )
+  const response = await instance.post<CreatePostResponse>('/posts/', params)
   return response.data
 }
 
 // 게시글 수정
 async function updatePostAPI(postId: string, params: UpdatePostRequest) {
-  const response = await instance.put(`/api/v1/posts/${postId}`, params)
+  const response = await instance.put(`/posts/${postId}`, params)
   return response.data
 }
 
 // 게시글 상세 조회
 async function getPostDetailAPI(postId: number) {
-  const response = await instance.get<PostDetailResponse>(
-    `/api/v1/posts/${postId}`
-  )
+  const response = await instance.get<PostDetailResponse>(`/posts/${postId}`)
   return response.data
 }
 
 // 좋아요
 async function likePostAPI(postId: number) {
-  const response = await instance.post(`/api/v1/posts/${postId}/like`)
+  const response = await instance.post(`/posts/${postId}/like`)
   return response.data
 }
 
 // 좋아요 취소
 async function unlikePostAPI(postId: number) {
-  const response = await instance.delete(`/api/v1/posts/${postId}/like`)
+  const response = await instance.delete(`/posts/${postId}/like`)
   return response.data
 }
 
