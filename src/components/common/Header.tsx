f69d3e195ade361ce3a4cbd85781f943d6fbba20
sysysysyb <baeksy6067@gmail.com>
2026-03-25 14:00:49 +0900

    refactor: 헤더 스타일링 변경 및 리다이렉트 url 적용 (#74)

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -8,7 +8,7 @@ function Header() {
         🚨 선착순 모집! 국비지원 받고 4주 완성
       </section>
 
-      <section className="flex h-16 w-full items-center justify-between px-90 text-lg whitespace-nowrap text-gray-600">
+      <section className="flex h-16 w-full min-w-170 items-center justify-between px-10 text-lg whitespace-nowrap text-gray-600 md:px-20 xl:px-90">
         <div className="flex items-center gap-15">
           <h1 className="shrink-0">
             <img className="h-auto w-30 object-contain" src={Logo} alt="logo" />
@@ -17,10 +17,10 @@ function Header() {
           <nav aria-label="주요 메뉴">
             <ul className="flex gap-15">
               <li className="py-4">
-                <Link to="/community">커뮤니티</Link>
+                <Link to="/posts">커뮤니티</Link>
               </li>
               <li className="py-4">
-                <Link to="/qna">질의응답</Link>
+                <a href="https://qna.ozcodingschool.site">질의응답</a>
               </li>
             </ul>
           </nav>
@@ -29,11 +29,11 @@ function Header() {
         <nav aria-label="사용자 메뉴">
           <ul className="flex items-center gap-2">
             <li className="py-4">
-              <Link to="/login">로그인</Link>
+              <a href="https://my.ozcodingschool.site/login">로그인</a>
             </li>
             <li className="text-xl">|</li>
             <li className="py-4">
-              <Link to="/signup">회원가입</Link>
+              <a href="https://my.ozcodingschool.site/signup">회원가입</a>
             </li>
           </ul>
         </nav>
