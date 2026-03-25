c21523f8149dca2ee3853755e5dca1be6184d727
김우진 <kedw158@gmail.com>
2026-03-25 14:31:18 +0900

    feat: 게시글 등록 API 연동 (#77)

--- src/api/apiInstance.ts
+++ src/api/apiInstance.ts
@@ -5,3 +5,19 @@ export const apiInstance = axios.create({
   baseURL: API_BASE_URL,
   withCredentials: true,
 })
+
+apiInstance.interceptors.request.use(
+  (config) => {
+    const token =
+      import.meta.env.VITE_TEMPORARY_ACCESS_TOKEN ||
+      localStorage.getItem('accessToken')
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
